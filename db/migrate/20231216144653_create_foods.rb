f42580b0aed6176b9b1d89091a42ed48901bea65
“sadaf <sadaf.batekzai54321@gmial.com>
2023-12-16 19:42:16 +0430

    create the user, food, recipe, and food_recipe tables

---
+++ db/migrate/20231216144653_create_foods.rb
@@ -0,0 +1,13 @@
+class CreateFoods < ActiveRecord::Migration[7.1]
+  def change
+    create_table :foods do |t|
+      t.string :name
+      t.string :measurement_unit
+      t.decimal :price
+      t.decimal :quantity
+      t.references :user, foreign_key: {to_table: :users}
+
+      t.timestamps
+    end
+  end
+end
