3c2df1fb44b8e02709d611ee0a470f1f8f890b58
Arch-Noize <santiagonoize@gmail.com>
2023-12-18 10:03:06 -0400

    Fix user sign up parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :require_login, unless: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 
   private
 
