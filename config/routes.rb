1a818d456e659d17e9202e7e2a3572dbd03d69b4
Arch-Noize <santiagonoize@gmail.com>
2023-12-18 09:42:27 -0400

    Change index page to login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  root to: "users#index"
+
   devise_for :users
- 
-  root "users#index"
+
   resources :users, only: [:index, :show] do
     resources :foods, only: [:index, :show]
     resources :recipes, only: [:index, :show] do
