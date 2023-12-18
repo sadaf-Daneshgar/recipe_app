8f09824e1d531eee50d943ef26ff131c33d2cdf5
“sadaf <sadaf.batekzai54321@gmial.com>
2023-12-18 18:16:18 +0430

    resolve the linter error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
-class ApplicationController < ActionController::Base 
+class ApplicationController < ActionController::Base
   before_action :require_login, unless: :devise_controller?
 
   private
 
   def require_login
-    unless user_signed_in?
-      redirect_to new_user_session_path
-    end
+    return if user_signed_in?
+
+    redirect_to new_user_session_path
   end
 end
