46d9035445e16a0f66aca6b0e397049d89ce5239
Arch-Noize <santiagonoize@gmail.com>
2023-12-19 07:44:06 -0400

    Add food functions

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,13 +1,26 @@
 class FoodsController < ApplicationController
   def index
-    @foods = Food.all
+    @user = User.find(params[:user_id])
+    @food = @user.foods
   end
 
-  def show
-    @food = Food.find_by_id(params[:id])
+  def new
+    @user = current_user
+    @food = @user.foods.build
+  end
 
-    return unless @food.nil?
+  def create
+    @food = current_user.foods.build(food_params)
+    if @food.save
+      redirect_to user_foods_path(current_user)
+    else
+      render :new
+    end
+  end
 
-    redirect_to foods_path
+  private 
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
   end
 end
