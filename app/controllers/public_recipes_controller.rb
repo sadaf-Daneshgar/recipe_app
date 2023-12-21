a0c2895ff44ff05d008be8e2e18434db67640b4b
Arch-Noize <santiagonoize@gmail.com>
2023-12-21 04:19:56 -0400

    Fix N+1 remaining issues

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,5 +1,5 @@
 class PublicRecipesController < ApplicationController
   def index
-    @p_recipes = Recipe.where(public: true).order(created_at: :desc)
+    @p_recipes = Recipe.includes([:user]).where(public: true).order(created_at: :desc)
   end
 end
