a934c329ac69afc1632ce73490e51ea994c9cf81
“sadaf <sadaf.batekzai54321@gmial.com>
2023-12-21 10:55:27 +0430

    solve the N+1 problem of food and recipe_foods

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,6 +1,6 @@
 class FoodsController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
+    @user = User.includes(:foods).find(params[:user_id])
     @food = @user.foods
   end
 
