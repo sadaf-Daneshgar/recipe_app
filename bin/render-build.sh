db7211a56489dcefd41868378cecd3e91e26e077
“sadaf <sadaf.batekzai54321@gmial.com>
2024-01-10 16:55:29 +0430

    update the files for rendering

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,10 +1,7 @@
+#!/usr/bin/env bash
 # exit on error
 set -o errexit
-
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
-bundle exec rails db:migrate
-
-#if you have seeds to run add:
-# bundle exec rails db:seed
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate 
